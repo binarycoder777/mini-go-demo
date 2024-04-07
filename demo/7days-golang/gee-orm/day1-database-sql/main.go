4099acc1d48499e708bbc51a998b92df0d49ab21
atao <87337334@qq.com>
2024-04-07 10:08:12 +0800

    add new code

---
+++ demo/7days-golang/gee-orm/day1-database-sql/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"7days-golang/gee-orm/day1-database-sql/orm"
+	"fmt"
+)
+
+func main() {
+	engine, _ := orm.NewEngine("sqlite3", "gee.db")
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	result, _ := s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	count, _ := result.RowsAffected()
+	fmt.Printf("Exec success, %d affected\n", count)
+}
---
+++ demo/7days-golang/gee-orm/day1-database-sql/orm/geeorm.go
@@ -0,0 +1,39 @@
+package orm
+
+import (
+	"7days-golang/gee-orm/day1-database-sql/log"
+	"7days-golang/gee-orm/day1-database-sql/session"
+	"database/sql"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{db: db}
+	log.Info("Connect database success")
+	return
+}
+
+// Close database connection
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+// NewSession creates a new session for next operations
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db)
+}
